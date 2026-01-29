864beb8b40fe7c37c33f4e3786f61c3969e8bf8c
Sports Prediction Admin <admin@sportsprediction.com>
2026-01-29 04:14:36 +0000

    Polish: Update services grid to 5 columns for better layout

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -50,7 +50,7 @@ export function Services() {
                 </div>
 
                 {/* Service Cards */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
                     {SERVICES.map((service, index) => (
                         <div
                             key={index}
